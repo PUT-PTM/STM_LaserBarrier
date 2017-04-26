e30b70a0ee4b7e308562cfb368d983fa3d5b953e
pawn-ch <krzysztof.orczyk@student.put.poznan.pl>
2017-04-26 21:04:15 +0200

    Safety first!
    Added safeties preventing the timer being triggered like a million times
    a second on account of ADC being crazy. Now the program looks for the
    first dip below the threshold and ignores the rest for a set short time
    allowing the light level to calm down and clear the threshold.
    Alternative would be to take an average over time from the light sensor
    which I might still do but I think that this is sufficient and much
    faster allowing for greater accuracy.

--- projekto/main.c
+++ projekto/main.c
@@ -7,18 +7,21 @@
 //#include "tm_stm32f4_adc.h"
 volatile double light;
 volatile double calibration;
-volatile double speed;
-volatile double distance;
-volatile double time;
-volatile long long int ms=0;
-volatile int first=0; //was this the first time the beam was interrupted?
-volatile int grace=0; //used to prevent mistaking a long object for a really fast object, grace makes the app wait for the laser to shine again on the sensor
+double speed=0;
+double distance=1;
+double time=0;
+long int ms=0;
+int first=0; //was this the first time the beam was interrupted?
+int grace=0; //used to prevent mistaking a long object for a really fast object, grace makes the app wait for the laser to shine again on the sensor
+long int wait=0; //waits a few cycles so the ifs don't get triggered like a million times a second on account of ADC being crazy
+int ignore=0; //if an if was triggered ignore everything for about 500ms for now will be tweaked later
+int ignoreduration=1000; //for how long ignore ifs after triggering
 
 void TIM3_IRQHandler(void){
 	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
 	{
 
-		ms++;								// a stupid hack, needs to be changed, there has to be a way to get the time elapsed from the registry ¯\_(ツ)_/¯
+		ms++;								// a stupid hack, needs to be changed, there has to be a way to get the time elapsed from the registry ÂŻ\_(ă�„)_/ÂŻ
 	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 	}
 }
@@ -29,49 +32,60 @@ int main(void)
 	SystemInit();
 	SystemCoreClockUpdate();
 	//TM_DELAY_Init();
-	TM_ADC_Init(ADC1, ADC_Channel_0);
-	TM_ADC_Init(ADC1, ADC_Channel_11);
+	TM_ADC_Init(ADC1, ADC_Channel_3);
+	TM_ADC_Init(ADC2, ADC_Channel_11);
 	Button_init();
 	TIM3_init(1,39999);
 	TIM3_inter();
-
+	while(1){ //begins only when pressed
+		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)==1){
+			break;
+		}
+	}
 	while(1){
 		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)==1){ //if the reset button was pressed just reset everything
 			first=0;
-			speed=0;
-			time=0;
+			speed=0.0;
+			time=0.0;
 			ms=0;
 			grace=0;
 
 		}
-		light=TM_ADC_Read(ADC1, ADC_Channel_0); //PA0			//brightness
-		calibration=TM_ADC_Read(ADC1, ADC_Channel_3); //PA3		//calibration connected to a potentiometer changes the threshold when the beam is considered as interrupted
+		light=TM_ADC_Read(ADC1, ADC_Channel_3); //PA3			//brightness
+		calibration=TM_ADC_Read(ADC2, ADC_Channel_11); //PC1		//calibration connected to a potentiometer changes the threshold when the beam is considered as interrupted
 
 		//$#$@$#%@
-		//######$$$$$$$$$ SOME WAY TO MAKE IT MORE ROBOUST, PERHAPS SOME KIND DEFENSE MECHANISM TO COMBAT RANDOM BRIGHTNESS CHANGES/AUTOMATIC BRIGHTNESS ADJUSTMENT#$$$$$$$$$$$$#############
+		//######$$$$$$$$$ SOME WAY TO MAKE IT MORE ROBOUST, PERHAPS SOME KIND OF DEFENSE MECHANISM TO COMBAT RANDOM BRIGHTNESS CHANGES/AUTOMATIC BRIGHTNESS ADJUSTMENT#$$$$$$$$$$$$#############
 		//$#$##$#$
-
-		if(light<calibration && grace==0){ //if the beam was intterupted and its not still being interrupted by the same object
-			if(first==0){	//if the beam was interrupted first time
-				//zacznij liczyc czas (zerujemy licznika i lecimy)
-				ms=0;
-				first=1;
+		if(!ignore){
+			if(light<calibration && grace==0){ //if the beam was intterupted and its not still being interrupted by the same object
+				if(first==0){	//if the beam was interrupted first time
+					//zacznij liczyc czas (zerujemy licznika i lecimy)
+					ms=0;
+					first=1;
+					wait=ms;
+					ignore=1;
+				}
+				else{			//if it was the second time the object appeared get time and calculate speed
+					//skoncz liczyc czas
+					time=ms/1000.0;
+					speed=distance/time;
+					first=0;
+					wait=ms;
+					ignore=1;
+				}
+				grace=1;		//makes sure to start the "stopwatch once"
+				// Waste some time //DONT
+				//for (int i = 0; i < 50000; i++); //NO
 			}
-			else{			//if it was the second time the object appeared get time and calculate speed
-				//skoncz liczyc czas
-				time=ms/1000;
-				speed=1/time;
-				first=0;
+			if(light>calibration && grace==1){ //if the beam is not interrupted and it's after it was interrupted
+				// Waste some time		//BAD PRACTICE
+				//for (int i = 0; i < 50000; i++);	// DONT DO THIS AT HOME KIDS
+				grace=0;	//goes back to normal
+				wait=ms;
+				ignore=1;
 			}
-			grace=1;		//makes sure to start the "stopwatch once"
-			// Waste some time //DONT
-			//for (int i = 0; i < 50000; i++); //NO
 		}
-		if(light>calibration && grace==1){ //if the beam is not interrupted and it's after it was interrupted
-			// Waste some time		//BAD PRACTICE
-			//for (int i = 0; i < 50000; i++);	// DONT DO THIS AT HOME KIDS
-			grace=0;					//goes back to normal
-		}
-
+		if(ms-wait>ignoreduration){ignore=0;} //if the time from triggering an if is greater than 500ms check for ifs
 	}
 }
