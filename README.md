a4994bef19e82036cb6d95cb6f87dec0143620ae
pawn-ch <krzysztof.orcz@gmail.com>
2017-06-07 19:11:47 +0200

    Finishing touches
    Changed the README and removed old code.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # STM_LaserBarrier
-A laser barrier utilising a simple light sensor and a laser pointer to measure the speed of an object.
+
+## Overview
+A laser barrier utilising a simple light sensor and a laser pointer to measure the speed of an object. Implemented on STM32 (STM32F407VG).
+## Description
+The board measures a speed of an object passing between a set of mirrors which reflect a single laser beam into a light a sensor. It measures the time elapsed between passing the first and the second laser beam and calculates the speed.
+Project components:
+- STM32 microcontroller (STM32F407VG or equivalent)
+![STM](readme/stm1.jpg?raw=true "STM")
+- 2x16 lcd display (LCD-03336)
+![LCD](readme/lcd1.jpg?raw=true "LCD")
+- fotoresistor (20-30 kohm)
+![FOT](readme/fot1.jpg?raw=true "FOT")
+- 2x potentiometers
+![POT](readme/pot1.jpg?raw=true "POT")
+
+
+## Tools
+- CoIDE v1.7.8
+- additional appropriate compilers and/or drivers for your board
+## How to run
+Connect all the components.  
+Pin layout:
+![https://stm32f4-discovery.net/2014/06/library-16-interfacing-hd44780-lcd-controller-with-stm32f4/](readme/lcd.png?raw=true "LCD")
+
+		
+STM32 | fotoresistor
+---|---
+PA3	|	out	
+
+
+STM32 | potentiometers
+---|---
+PC1	|	ADC1
+V0(LCD)	|	ADC2
+
+## How to compile
+Compile using CoIDE and GCC compiler and run on the board.
+## Future improvements
+Better sensor, more robust and advanced object detection algorithm. 
+## Attributions
+Libraries for ADC, DELAY, GPIO and HD44780 can be found over [here](https://stm32f4-discovery.net/)
+## License
+Code licensed under the The MIT License.
+
+
+
+The project was conducted during the Microprocessor Lab course held by the Institute of Control and Information Engineering, Poznan University of Technology.
+Supervisor: Adam Bondyra
--- projekto/main.c
+++ projekto/main.c
@@ -1,28 +1,35 @@
+#include "stdio.h"
 #include "stm32f4xx_conf.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_adc.h"
 #include "stm32f4xx_tim.h"
-//#include "biblioteka.h"
-//#include "tm_stm32f4_adc.h"
-volatile double light;
-volatile double calibration;
+#include "defines.h"
+#include "tm_stm32f4_delay.h"
+#include "tm_stm32f4_hd44780.h"
+int light;
+int calibration;
 double speed=0;//m/s
 double distance=1;
 double time=0;
 long int ms=0;
 int first=0; //was this the first time the beam was interrupted?
 int grace=0; //used to prevent mistaking a long object for a really fast object, grace makes the app wait for the laser to shine again on the sensor
-long int wait=0; //waits a few cycles so the ifs don't get triggered like a million times a second on account of ADC being crazy
-int ignore=0; //if an if was triggered ignore everything for about 500ms for now will be tweaked later
-int ignoreduration=500; //for how long ignore ifs after triggering
+long int wait=0; //waits a few cycles so the ifs don't get triggered a million times a second on account of ADC being crazy
+int ignore=0; //if an if was triggered ignore everything for about Xms (100ms is enough to measure speeds up to 5m/s)
+int ignoreduration=100; //for how long to ignore ifs after triggering
+char timems[6];
+char speeds[4];
+char lights[6];
+char calibrations[6];
+
 
 void TIM3_IRQHandler(void){
 	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
 	{
 
-		ms++;								// a stupid hack, needs to be changed, there has to be a way to get the time elapsed from the registry Ă‚Ĺ»\_(Ä�ďż˝â€ž)_/Ă‚Ĺ» BUT IT WERKS
-	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+		ms++;
+		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 	}
 }
 
@@ -31,17 +38,34 @@ int main(void)
 
 	SystemInit();
 	SystemCoreClockUpdate();
-	//TM_DELAY_Init();
+	TM_DELAY_Init();
+	TM_HD44780_Init(16, 2);
+
 	TM_ADC_Init(ADC1, ADC_Channel_3);
 	TM_ADC_Init(ADC2, ADC_Channel_11);
+
 	Button_init();
+
 	TIM3_init(1,39999);
 	TIM3_inter();
+
+	TM_HD44780_Clear();
+	TM_HD44780_Puts(0, 0, "Ready to start");
+	TM_HD44780_Puts(0, 1, "BRI::");
+	TM_HD44780_Puts(8, 1, "CAL:");
 	while(1){ //begins only when pressed
 		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)==1){
 			break;
 		}
+		light=TM_ADC_Read(ADC1, ADC_Channel_3); //PA3			//brightness
+		calibration=TM_ADC_Read(ADC2, ADC_Channel_11)+50; //PC1		//calibration connected to a potentiometer changes the threshold when the beam is considered as interrupted
+
+		sprintf(lights, "%d", light);
+		TM_HD44780_Puts(4, 1, lights);
+		sprintf(calibrations, "%d", calibration);
+		TM_HD44780_Puts(12, 1, calibrations);
 	}
+	TM_HD44780_Clear();
 	while(1){
 		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)==1){ //if the reset button was pressed just reset everything
 			first=0;
@@ -49,43 +73,46 @@ int main(void)
 			time=0.0;
 			ms=0;
 			grace=0;
-
+			TM_HD44780_Clear();
 		}
 		light=TM_ADC_Read(ADC1, ADC_Channel_3); //PA3			//brightness
 		calibration=TM_ADC_Read(ADC2, ADC_Channel_11)+50; //PC1		//calibration connected to a potentiometer changes the threshold when the beam is considered as interrupted
 
-		//$#$@$#%@
-		//######$$$$$$$$$ SOME WAY TO MAKE IT MORE ROBOUST, PERHAPS SOME KIND OF DEFENSE MECHANISM TO COMBAT RANDOM BRIGHTNESS CHANGES/AUTOMATIC BRIGHTNESS THRESHOLD ADJUSTMENT#$$$$$$$$$$$$#############
-		//$#$##$#$
 		if(!ignore){
-			if(light>calibration && grace==0){ //if the beam was interrupted and its not still being interrupted by the same object
+			if(light>calibration && grace==0){ //if the beam was interrupted and it's not still being interrupted by the same object
 				if(first==0){	//if the beam was interrupted first time
-					//zacznij liczyc czas (zerujemy licznika i lecimy)
 					ms=0;
 					first=1;
 					wait=ms;
 					ignore=1;
+					TM_HD44780_Clear();
+					sprintf(speeds, "%f", speed);
+				    TM_HD44780_Puts(3, 1, speeds);
+				    TM_HD44780_Puts(13, 0, "raz");
 				}
 				else{			//if it was the second time the object appeared get time and calculate speed
-					//skoncz liczyc czas
 					time=ms/1000.0;
 					speed=distance/time;
 					first=0;
 					wait=ms;
 					ignore=1;
+				    TM_HD44780_Puts(13, 1, "dwa");
+					sprintf(speeds, "%f", speed);
+				    TM_HD44780_Puts(3, 1, speeds);
 				}
 				grace=1;		//makes sure to start the "stopwatch once"
-				// Waste some time //DONT
-				//for (int i = 0; i < 50000; i++); //NO
 			}
 			if(light<calibration && grace==1){ //if the beam is not interrupted and it's after it was interrupted
-				// Waste some time		//BAD PRACTICE
-				//for (int i = 0; i < 50000; i++);	// DONT DO THIS AT HOME KIDS
 				grace=0;	//goes back to normal
 				wait=ms;
 				ignore=1;
 			}
 		}
 		if(ms-wait>ignoreduration){ignore=0;} //if the time from triggering an if is greater than ignoreduration check for ifs
+	    TM_HD44780_Puts(0, 0, "T:");
+	    TM_HD44780_Puts(0, 1, "S:");
+		sprintf(timems, "%d", ms);
+	    TM_HD44780_Puts(3, 0, timems);
+
 	}
 }
